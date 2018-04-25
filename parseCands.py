0696fb4daf8cd5d0d985794936714887ec3e0d8c
Luke <lukemills88@gmail.com>
2018-04-24 20:15:29 -0500

    Complete parse of first file

--- parseCands.py
+++ parseCands.py
@@ -8,7 +8,7 @@ def getFirstLast(nc1):
 
 	# Remove party affiliation	
 
-	match = re.search(r'(.)', strs[ns-1])
+	match = re.search(r'\(.\)', strs[ns-1])
 	if(len(strs[ns-1]) > 3 and match):
 		strs[ns-1] = strs[ns-1][:-3]
 		print(strs[ns-1])
@@ -25,17 +25,60 @@ def getFirstLast(nc1):
 		strs.pop()
 
 	ns = len(strs)
-	if(ns == 3):
-		firstLast = strs[0] + " " + strs[2]
+
+	print("Starting on ", strs)
+	for i in range(0, ns):
+		if(i > ns-1):
+			break
+		print("\t", i, ":", strs)
+
+		#lw is last word in string nc1
+		lw = strs[i].strip('.').lower() 
+
+		if(len(lw) == 1):
+			del strs[i]
+			ns = len(strs)
+			i = i-1
+		if(lw == "sr" or lw == "jr" or lw == "i" or lw == "ii" or lw == "iii" or lw == "iv" or lw == "v" or lw == "vi" or lw == "vii"):
+			print("Deleting", lw)
+			del strs[i]
+			ns = len(strs)
+			i = i-1
+		else:
+			match = re.search(r'\(.\)', lw)
+			if(len(strs[i]) > 3 and match):
+				strs[i] = strs[i][:-3]
+				print(strs[i])
+			elif(ns > 1 and match):
+				del strs[i]
+				ns = len(strs)
+				i = i-1
+			else:
+				match = re.search(r'\(.*\)', lw)
+				if(match):
+					del strs[i]
+					ns = len(strs)
+					i = i-1
+
+	ns = len(strs)
+	if(ns == 1):
+		firstLast = strs[0]
 	elif(ns == 2):
 		firstLast = strs[0] + " " + strs[1]
+	else:
+		firstLast = ""
+		for i in range(0, ns):
+			firstLast += strs[i]
+
+	return firstLast
+'''
+	elif(ns == 3):
+		firstLast = strs[0] + " " + strs[2]
 	elif(ns == 4):
 		firstLast = strs[0] + " " + strs[3]
-	else:
-		firstLast = "Unknown"
-		print("\tError:", nc1, strs)
+'''
+	
 		
-	return firstLast
 		
 	
 
@@ -46,6 +89,7 @@ c2 = open(sys.argv[2], "r")
 out = open(sys.argv[3], "w")
 
 reader = csv.DictReader(c1, delimiter=';')
+# getFirstLastTwo("foo bar is cool")
 for row in reader:
 	nc1 = row['name'] 
 	print(getFirstLast(nc1))
